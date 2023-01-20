c624c12fe026ed18dbe314098c0f2a4a3b043b25
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2023-01-20 01:37:09 -0500

    Finished SystemInfo, Error opening utmp

--- main.c
+++ main.c
@@ -3,20 +3,127 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include <sys/utsname.h>
+#include <sys/resource.h>
+#include <sys/sysinfo.h>
+#include <sys/types.h>
+#include <utmp.h>
+#include <unistd.h>
+
 void sysInfo()
 {
-    printf("System Usage");
+    printf("### System Usage ###\n");
+
+    //     struct utsname
+    //     {
+    //         char sysname[];  /* Operating system name (e.g., "Linux") */
+    //         char nodename[]; /* Name within "some implementation-defined
+    //                             network" */
+    //         char release[];  /* Operating system release
+    //                             (e.g., "2.6.28") */
+    //         char version[];  /* Operating system version */
+    //         char machine[];  /* Hardware identifier */
+    // #ifdef _GNU_SOURCE
+    //         char domainname[]; /* NIS or YP domain name */
+    // #endif
+    //     };
+
+    struct utsname sys_info;
+    uname(&sys_info);
+    printf("Success getting system info!\n\n");
+    printf("System Name:\t%s\n", sys_info.sysname);
+    printf("Machine Name:\t%s\n", sys_info.nodename);
+    printf("Version:\t%s\n", sys_info.version);
+    printf("Release:\t%s\n", sys_info.release);
+#ifdef _GNU_SOURCE
+    printf("Domain Name:\t%s\n", sys_info.domainname);
+#endif
+    printf("Architecture:\t%s\n", sys_info.machine);
 }
 
+// Session/User Info
 void userInfo()
 {
-    printf("User Usage");
+    printf("### User Usage ###\n");
+
+    // struct exit_status {              /* Type for ut_exit, below */
+    //     short e_termination;          /* Process termination status */
+    //     short e_exit;                 /* Process exit status */
+    // };
+
+    // struct utmp {
+    //     short   ut_type;              /* Type of record */
+    //     pid_t   ut_pid;               /* PID of login process */
+    //     char    ut_line[UT_LINESIZE]; /* Device name of tty - "/dev/" */
+    //     char    ut_id[4];             /* Terminal name suffix,
+    //                                     or inittab(5) ID */
+    //     char    ut_user[UT_NAMESIZE]; /* Username */
+    //     char    ut_host[UT_HOSTSIZE]; /* Hostname for remote login, or
+    //                                     kernel version for run-level
+    //                                     messages */
+    //     struct  exit_status ut_exit;  /* Exit status of a process
+    //                                     marked as DEAD_PROCESS; not
+    //                                     used by Linux init(1) */
+    //     /* The ut_session and ut_tv fields must be the same size when
+    //         compiled 32- and 64-bit.  This allows data files and shared
+    //         memory to be shared between 32- and 64-bit applications. */
+    // #if __WORDSIZE == 64 && defined __WORDSIZE_COMPAT32
+    //     int32_t ut_session;           /* Session ID (getsid(2)),
+    //                                     used for windowing */
+    //     struct {
+    //         int32_t tv_sec;           /* Seconds */
+    //         int32_t tv_usec;          /* Microseconds */
+    //     } ut_tv;                      /* Time entry was made */
+    // #else
+    //     long   ut_session;           /* Session ID */
+    //     struct timeval ut_tv;        /* Time entry was made */
+    // #endif
+
+    //     int32_t ut_addr_v6[4];        /* Internet address of remote
+    //                                     host; IPv4 address uses
+    //                                     just ut_addr_v6[0] */
+    //     char __unused[20];            /* Reserved for future use */
+    // };
+
+    struct utmp user_info;
+    struct utmp *user_info_ptr = &user_info;
+    // Open utmp file
+    FILE *file = fopen("/var/run/utmp", "r");
+    if (file == NULL)
+    {
+        printf("Error opening utmp file!\n");
+        return -1;
+    }
+    // Get user info
+    user_info_ptr = getutent();
+    printf("Success getting user info!\n\n");
+    printf("%d\n", getutent_r(user_info_ptr, &user_info_ptr));
+    
+        printf("User:\t%s\t", user_info.ut_user);
+        printf("PID:\t%d\t", user_info.ut_pid);
+        printf("Session:\t%d\t", user_info.ut_session); //%ld
+
+    setutent();
+
+    while (getutent_r(user_info_ptr, &user_info_ptr) != -1)
+    {
+        // Print Session/User Info
+        printf("User:\t%s\t", user_info.ut_user);
+        printf("PID:\t%d\t", user_info.ut_pid);
+        printf("Session:\t%d\t", user_info.ut_session); //%ld
+        printf("Time:\t%d\t", user_info.ut_tv.tv_sec); //%ld
+        printf("Host:\t%s\t", user_info.ut_host);
+        printf("Line:\t%s\t", user_info.ut_line);
+        printf("ID:\t%s\t", user_info.ut_id);
+
+        printf("\n");
+    }
 }
 
 void graph(bool seq, int samples, double tdelay)
 {
     // testing args
-    printf("seq: %d samples: %d tdelay: %f", seq, samples, tdelay);
+    printf("seq: %d samples: %d tdelay: %f\n", seq, samples, tdelay);
 }
 
 int main(int argc, char *argv[])
@@ -123,11 +230,9 @@ int main(int argc, char *argv[])
             // ...
 
             graph(seq, samples, tdelay);
-
         }
         return 0;
     }
-
 
     printf("Invalid argument: %s\n", argv[1]);
     printf("Usage: %s [arguments] [options]\n", argv[0]);
